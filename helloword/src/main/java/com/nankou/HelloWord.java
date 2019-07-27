014559e45293bed09501ed342e84b1b8375cd8c3
Administrator <Administrator@192.168.42.1>
2019-07-27 12:38:43 +0800

    添加了 file等信息

--- helloword/src/main/java/com/nankou/HelloWord.java
+++ helloword/src/main/java/com/nankou/HelloWord.java
@@ -1,5 +1,7 @@
 package com.nankou;
 
+import java.io.File;
+
 public class HelloWord {
 
 	public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class HelloWord {
 
 		System.out.println("hello world");
 		System.out.println("挣大钱。。。");
+		File file=null;
 	}
 
 }
